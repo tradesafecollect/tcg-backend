f3c62b76a24a99d22e6c1601fbc3782d7649aaec
tradesafecollect <tradesafecollect@gmail.com>
2026-06-01 01:36:04 +0200

    add mailer + email system

---
+++ src/utils/mailer.js
@@ -0,0 +1,25 @@
+const nodemailer = require('nodemailer');
+
+const transporter = nodemailer.createTransport({
+    service: 'gmail',
+    auth: {
+        user: process.env.EMAIL_USER,
+        pass: process.env.EMAIL_PASS
+    }
+});
+
+exports.sendBetaNotification = async (data) => {
+    await transporter.sendMail({
+        from: process.env.EMAIL_USER,
+        to: process.env.EMAIL_USER,
+        subject: 'Nuova richiesta beta',
+        html: `
+            <h2>Nuova richiesta beta</h2>
+            <p><b>Username:</b> ${data.username}</p>
+            <p><b>Email:</b> ${data.email}</p>
+            <p><b>Nome:</b> ${data.nome} ${data.cognome}</p>
+            <p><b>Città:</b> ${data.citta}</p>
+            <p><b>Anni collezionismo:</b> ${data.anni_collezionismo}</p>
+        `
+    });
+};
